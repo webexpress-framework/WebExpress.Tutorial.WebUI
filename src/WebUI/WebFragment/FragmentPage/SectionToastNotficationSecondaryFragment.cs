8a7f99d4e223c344669853f38675ec313c93fdd2
Rene Schwarzer <rene_schwarzer@hotmail.de>
2025-09-22 22:14:50 +0200

    feat: add toast fragments, general improvements and minor bugs

---
+++ src/WebUI/WebFragment/FragmentPage/SectionToastNotficationSecondaryFragment.cs
@@ -0,0 +1,26 @@
+﻿using WebExpress.WebApp.WebSection;
+using WebExpress.WebCore.WebAttribute;
+using WebExpress.WebCore.WebFragment;
+using WebExpress.WebUI.WebFragment;
+
+namespace WebExpress.Tutorial.WebUI.WebFragment.FragmentPage
+{
+    /// <summary>
+    /// Represents a fragment control for the tutorial.
+    /// </summary>
+    [Section<SectionToastNotficationSecondary>]
+    [Scope<IScopeFragment>]
+    [Cache]
+    public sealed class SectionToastNotficationSecondaryFragment : FragmentControlText
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        /// <param name="fragmentContext">The context in which the fragment is used.</param>
+        public SectionToastNotficationSecondaryFragment(IFragmentContext fragmentContext)
+            : base(fragmentContext)
+        {
+            Text = "SectionToastNotficationSecondary";
+        }
+    }
+}
