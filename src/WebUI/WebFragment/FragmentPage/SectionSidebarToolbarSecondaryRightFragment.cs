87cbb1e19e81f25665385d1b7cd69819bc2826f9
Rene Schwarzer <rene_schwarzer@hotmail.de>
2025-10-05 09:19:58 +0200

    feat: improved sidebar control and minor bugs

---
+++ src/WebUI/WebFragment/FragmentPage/SectionSidebarToolbarSecondaryRightFragment.cs
@@ -0,0 +1,28 @@
+﻿using WebExpress.WebApp.WebSection;
+using WebExpress.WebCore.WebAttribute;
+using WebExpress.WebCore.WebFragment;
+using WebExpress.WebUI.WebControl;
+using WebExpress.WebUI.WebFragment;
+
+namespace WebExpress.Tutorial.WebUI.WebFragment.FragmentPage
+{
+    /// <summary>
+    /// Represents a fragment control for the tutorial.
+    /// </summary>
+    [Section<SectionSidebarToolbarSecondary>]
+    [Scope<IScopeFragment>]
+    [Cache]
+    public sealed class SectionSidebarToolbarSecondaryRightFragment : FragmentControlToolbarItemButton
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        /// <param name="fragmentContext">The context in which the fragment is used.</param>
+        public SectionSidebarToolbarSecondaryRightFragment(IFragmentContext fragmentContext)
+            : base(fragmentContext)
+        {
+            Alignment = TypeToolbarItemAlignment.Right;
+            Text = "SectionSidebarToolbarSecondary (right)";
+        }
+    }
+}
