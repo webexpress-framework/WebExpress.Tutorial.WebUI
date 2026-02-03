da3a856e31b6ddcb58785d4278960e6b4052c2fa
Rene Schwarzer <rene_schwarzer@hotmail.de>
2026-02-03 20:54:59 +0100

    feat: general improvements and minor bugs

--- src/WebUI/WWW/Api/_1_/MonkeyIslandBoatWql.cs
+++ src/WebUI/WWW/Api/_1_/MonkeyIslandBoatWql.cs
@@ -1,6 +1,8 @@
-﻿using WebExpress.Tutorial.WebUI.Model;
+﻿using System.Collections.Generic;
+using WebExpress.Tutorial.WebUI.Model;
 using WebExpress.WebApp.WebRestApi;
 using WebExpress.WebCore.WebApplication;
+using WebExpress.WebCore.WebMessage;
 using WebExpress.WebCore.WebSitemap;
 
 namespace WebExpress.Tutorial.WebUI.WWW.Api._1_
@@ -17,6 +19,27 @@ namespace WebExpress.Tutorial.WebUI.WWW.Api._1_
         /// <param name="applicationContext">The application context containing the current state of the application.</param>
         public MonkeyIslandBoatWql(ISitemapManager sitemapManager, IApplicationContext applicationContext)
         {
+        }
+
+        /// <summary>
+        /// Retrieves a collection of historical entries associated with the specified request.
+        /// </summary>
+        /// <remarks>
+        /// Override this method in a derived class to provide custom history retrieval
+        /// logic.
+        /// </remarks>
+        /// <param name="request">
+        /// The request for which to retrieve history. Cannot be null.
+        /// </param>
+        /// <returns>
+        /// An enumerable collection of strings representing the history entries for the 
+        /// specified request. The collection is empty if no history is available.
+        /// </returns>
+        protected override IEnumerable<string> GetHistory(IRequest request)
+        {
+            yield return "Name ~ \"Monkey\" AND BoatType ~ \"Ghost Ship\"";
+            yield return "BoatType ~ \"Ghost Ship\"";
+            yield return "Name ~ \"Monkey\"";
         }
     }
 }
